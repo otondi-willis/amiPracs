f6e8854e3841ce3afa66e5f0b985c350f1d62d5b
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-08 14:36:22 +0300

    java springboot file

---
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.amigos.learning_springbootAmi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringbootAmiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningSpringbootAmiApplication.class, args);
+	}
+
+}
