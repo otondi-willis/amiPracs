163367179c354d6b2bd35bd6d1ffb9ef255e5b94
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-08 14:36:09 +0300

    java test file

---
+++ src/test/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootlearning.amigos.learning_springbootAmi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringbootAmiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
