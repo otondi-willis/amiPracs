4cec15f0bfc63f1224483f37d47d2ec06f258702
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-08 14:58:08 +0300

    use of @RestController and @GetMapping

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
@@ -2,12 +2,20 @@ package com.springbootlearning.amigos.learning_springbootAmi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class LearningSpringbootAmiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringbootAmiApplication.class, args);
+	}
+
+	@GetMapping //to annotate as RESTful
+	public String hello(){
+		return "Hello World";
 	}
 
 }
