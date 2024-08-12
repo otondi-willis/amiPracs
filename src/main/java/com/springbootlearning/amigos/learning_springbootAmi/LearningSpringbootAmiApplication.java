5d0cbed119c5b385ddf820fb1eca367e4d2f39b3
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 20:57:23 +0300

    removed @RestController annotation

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
@@ -11,22 +11,13 @@ import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
-@RestController //makes the class to serve REST endpoints
+
 public class LearningSpringbootAmiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LearningSpringbootAmiApplication.class, args);
 	}
 
-	@GetMapping //to annotate as RESTful
-	public List<Student> hello(){
-		return List.of(
-				new Student(2L,
-				"Willis",
-				"willis.otondi@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21)
-		);
-	}
+
 
 }
