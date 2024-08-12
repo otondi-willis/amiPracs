65580d8a747c78eeab4a2fd5ad6eb6f087d97eac
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 20:40:12 +0300

    added private attributes

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/Student.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/Student.java
@@ -1,4 +1,11 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import java.time.LocalDate;
+
 public class Student {
+    private Long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
 }
