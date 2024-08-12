780caa6ed104f393260a7cc5e87e87e0aaa46814
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 20:59:54 +0300

    use @RequestMapping annotation in student controller

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentController.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentController.java
@@ -1,7 +1,9 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(path = "api/v1/student")
 public class StudentController {
 }
