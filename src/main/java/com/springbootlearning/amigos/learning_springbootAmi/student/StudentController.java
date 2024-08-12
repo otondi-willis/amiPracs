871dcc181bfd080e09896f14adee3005c18290b3
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 20:58:20 +0300

    use @RestController annotation in student controller

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentController.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentController.java
@@ -1,4 +1,7 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
 }
