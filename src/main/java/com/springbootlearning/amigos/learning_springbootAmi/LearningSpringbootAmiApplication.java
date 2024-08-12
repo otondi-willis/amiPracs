781cb8a23c7bcb80076bbcfd64cba79368685003
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 20:54:47 +0300

    changed from list of strings to list of students

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
@@ -1,10 +1,13 @@
 package com.springbootlearning.amigos.learning_springbootAmi;
 
+import com.springbootlearning.amigos.learning_springbootAmi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +19,14 @@ public class LearningSpringbootAmiApplication {
 	}
 
 	@GetMapping //to annotate as RESTful
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(2L,
+				"Willis",
+				"willis.otondi@gmail.com",
+				LocalDate.of(2000, Month.JANUARY, 5),
+				21)
+		);
 	}
 
 }
