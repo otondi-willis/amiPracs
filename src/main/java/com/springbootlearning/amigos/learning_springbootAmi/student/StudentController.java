4c84d7f2e4eacd2e7d5507e613c78b69c4383971
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-12 21:00:36 +0300

    added @GetMapping annotation

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentController.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentController.java
@@ -1,9 +1,24 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+    @GetMapping //to annotate as RESTful
+    public List<Student> hello(){
+        return List.of(
+                new Student(2L,
+                        "Willis",
+                        "willis.otondi@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21)
+        );
+    }
 }
