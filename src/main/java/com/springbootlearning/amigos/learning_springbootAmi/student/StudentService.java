00fb79c836189e7cd102ab05d3291d4799e54f38
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-13 21:36:25 +0300

    use of Component

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
@@ -1,9 +1,12 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import org.springframework.stereotype.Component;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+@Component
 public class StudentService {
     public List<Student> getStudents(){
         return List.of(
