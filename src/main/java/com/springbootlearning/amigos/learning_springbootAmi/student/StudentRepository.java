30de1323e653de877e75b94f7b7631eccab91ba0
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-13 22:45:57 +0300

    added generics...object to work with and the id type

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentRepository.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentRepository.java
@@ -2,6 +2,7 @@ package com.springbootlearning.amigos.learning_springbootAmi.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository {
+public interface StudentRepository
+        extends JpaRepository<Student, Long> {
 
 }
