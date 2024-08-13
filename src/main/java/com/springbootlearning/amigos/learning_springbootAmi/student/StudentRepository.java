755c9bff5d8ea2117a7996d1d34a00eabdfcb69f
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-13 22:46:41 +0300

    use of @Repository

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentRepository.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentRepository.java
@@ -1,7 +1,9 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository
         extends JpaRepository<Student, Long> {
 
