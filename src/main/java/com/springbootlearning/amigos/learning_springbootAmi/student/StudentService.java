640729f382f934af14dcb7558bddc3dd02e01a92
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-13 21:08:40 +0300

    created getStudents method in StudentService class

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
@@ -1,4 +1,17 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 public class StudentService {
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(2L,
+                        "Willis",
+                        "willis.otondi@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21)
+        );
+    }
 }
