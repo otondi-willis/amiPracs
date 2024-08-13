d6b5390878bf44b9f2f977c2d31e87f007e23dc1
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-13 08:37:31 +0300

    removed unused imports

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/LearningSpringbootAmiApplication.java
@@ -1,14 +1,8 @@
 package com.springbootlearning.amigos.learning_springbootAmi;
 
-import com.springbootlearning.amigos.learning_springbootAmi.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.List;
 
 @SpringBootApplication
 
