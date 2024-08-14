6dff10637cf9bcf5ae9031d651830131d12822a8
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-14 20:52:52 +0300

    annotated with @Bean

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
@@ -1,10 +1,12 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StudentConfig {
+    @Bean
     CommandLineRunner commandLineRunner(){
         return args -> {};
     }
