77fc737796116f0770a67a244259d6a7627662d0
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-14 20:47:00 +0300

    use of findAll() to get a list

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
@@ -18,12 +18,6 @@ public class StudentService {
     }
 
     public List<Student> getStudents(){
-        return List.of(
-                new Student(2L,
-                        "Willis",
-                        "willis.otondi@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21)
-        );
+       return studentRepository.findAll();
     }
 }
