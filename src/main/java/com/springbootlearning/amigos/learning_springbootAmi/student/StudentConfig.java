00d1c66e2e018b2e3ee510c16624cddfd178d037
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-14 20:50:49 +0300

    annotated with @Configuration

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
@@ -1,4 +1,7 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class StudentConfig {
 }
