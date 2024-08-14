c3a0fdd5fe55ba77277ec49c174aaa8492ba9d51
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-14 20:52:14 +0300

    use of CommandLinerRunner

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
@@ -1,7 +1,11 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StudentConfig {
+    CommandLineRunner commandLineRunner(){
+        return args -> {};
+    }
 }
