e7b73db7c8c73f24b58993769f1f6bc4cd0166ed
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-14 20:53:51 +0300

    added repository as the args

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
@@ -4,10 +4,19 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 @Configuration
 public class StudentConfig {
     @Bean
-    CommandLineRunner commandLineRunner(){
-        return args -> {};
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            new Student(2L,
+                    "Willis",
+                    "willis.otondi@gmail.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21)
+        };
     }
 }
