a2f67d7b114cae45df5934066baee61360de7531
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-19 12:02:30 +0300

    added students and saved them in the repository

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 import static java.util.Calendar.JANUARY;
 
@@ -14,11 +15,20 @@ public class StudentConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository repository){
         return args -> {
-            new Student(
+            Student willis = new Student(
                     "Willis",
                     "willis.otondi@gmail.com",
                     LocalDate.of(2000, JANUARY, 5),
                     21);
+            Student alice = new Student(
+                    "alice",
+                    "alice.otondi@gmail.com",
+                    LocalDate.of(2001, JANUARY, 5),
+                    21);
+
+            repository.saveAll(
+                    List.of(willis, alice)
+            );
         };
     }
 }
