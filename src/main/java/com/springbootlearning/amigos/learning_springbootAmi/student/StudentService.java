7277c5b0258b27cfe0ea76491b15c9330c2d325b
otondi-willis <willis.otondi.ke@gmail.com>
2024-08-26 05:26:36 +0300

    @Transactional in action

--- src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
+++ src/main/java/com/springbootlearning/amigos/learning_springbootAmi/student/StudentService.java
@@ -1,13 +1,16 @@
 package com.springbootlearning.amigos.learning_springbootAmi.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service //changed from Component for semantics
@@ -41,5 +44,23 @@ public class StudentService {
 
         }
         studentRepository.deleteById(studentId);
+    }
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+        Student student = studentRepository.findById(studentId)
+                .orElseThrow(()->new IllegalStateException(
+                        "student with id" + studentId + "does not exist"
+                ));
+        if(name != null && name.length()>0 && !Objects.equals(student.getName(),name)){
+            student.setName(name);
+        }
+        if(email != null && email.length()>0 && !Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentOptional = studentRepository
+                    .findStudentByEmail(email);
+            if(studentOptional.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
